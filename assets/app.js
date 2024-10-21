136e9ed17ab6e5549c4dee58d2d4d4b07f85afe1
Atomic207 <elijah.rietmann@icloud.com>
2024-10-21 14:16:44 +0200

    commit crime

--- assets/app.js
+++ assets/app.js
@@ -2,18 +2,23 @@
 const stationElement = document.getElementById("station");
 const buttonElement = document.getElementById("submit");
 const tableElement = document.getElementById("table-body");
-// opendata api URL
-const url = `https://transport.opendata.ch/v1/stationboard?station=${station}`;
-
-const getData = async () => {
-  const station = stationElement.value;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-};
 
 buttonElement.addEventListener("click", async () => {
   const data = await getData();
   displayData(data);
 });
-const displayData = (data) => {};
+// opendata api URL
+
+const getData = async () => {
+  const station = stationElement.value;
+  const url = `https://transport.opendata.ch/v1/stationboard?station=${station}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+const displayData = (data) => {
+  const stationName = data.station.name;
+    const stationNameElement = document.getElementById("station-name");
+    stationNameElement.textContent = stationName;                                   
+};
