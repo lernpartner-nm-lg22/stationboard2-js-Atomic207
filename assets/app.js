e5f240b38209b8378a76f7a985c26bbdbbb77663
Atomic207 <elijah.rietmann@icloud.com>
2024-10-21 13:51:47 +0200

    data data

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,19 @@
-"use strict"
+"use strict";
+const stationElement = document.getElementById("station");
+const buttonElement = document.getElementById("submit");
+const tableElement = document.getElementById("table-body");
 // opendata api URL
 const url = `https://transport.opendata.ch/v1/stationboard?station=${station}`;
 
-const getData = async () => {};
+const getData = async () => {
+  const station = stationElement.value;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
 
+buttonElement.addEventListener("click", async () => {
+  const data = await getData();
+  displayData(data);
+});
 const displayData = (data) => {};
